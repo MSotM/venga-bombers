1976b3b26187b7ad741dcead56b149d41d430b0b
Joram Schrijver <i@joram.io>
2017-01-09 23:19:02 +0100

    Fix bitvalues for seven segment id (#92)

--- src/seven-segment-renderer.c
+++ src/seven-segment-renderer.c
@@ -12,12 +12,12 @@ void render_to_seven_segment(uint8_t number) {
   if (number & 1) {
     PORTC |= _BV(PORTC0);
   } else {
-    PORTC &= _BV(PORTC0);
+    PORTC &= ~_BV(PORTC0);
   }
 
   if (number & 2) {
     PORTC |= _BV(PORTC1);
   } else {
-    PORTC &= _BV(PORTC1);
+    PORTC &= ~_BV(PORTC1);
   }
 }
